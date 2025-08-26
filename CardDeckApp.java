509ecf4c7f1704a2edee5947d3063f96602aa86d
JaredLove <lovejared91@Yahoo.com>
2025-08-26 07:30:35 -0500

    added logic to create the deck of cards

--- CardDeckApp.java
+++ CardDeckApp.java
@@ -1,23 +1,48 @@
+/******************************************************************************
+Programmer: Jared Love 
+Date: 08/31/2025
+Lab X 
+Instructor: Dr. Rafael Azuaje
+College: San Antonio College or other from Alamo Colleges District
+
+*******************************************************************************/
+
+import java.util.Arrays; // Import the Arrays class
 public class CardDeckApp {
 
     public static void main(String[] args) {
         System.out.println("DECK");
         String[] deck = getDeck();
-        displayCards(deck);
+        System.out.println(Arrays.toString(deck));
+        // displayCards(deck);
 
-        System.out.println("SHUFFLED DECK");
-        shuffleDeck(deck);
-        displayCards(deck);
+        // System.out.println("SHUFFLED DECK");
+        // shuffleDeck(deck);
+        // displayCards(deck);
 
-        int count = 2;
-        System.out.println("HAND OF " + count + " CARDS");
-        String[] hand = dealCards(deck, count);
-        displayCards(hand);
+        // int count = 2;
+        // System.out.println("HAND OF " + count + " CARDS");
+        // String[] hand = dealCards(deck, count);
+        // displayCards(hand);
     }
 
     private static String[] getDeck() {
         String[] deck = new String[52];
         // add code that creates deck here
+            //suites string array
+        String[] suites = {"Spades", "Hearts", "Clubs", "Diamonds"};
+            //ranks string array
+        String[] rank = {"Ace Of ", "2 Of ", "3 Of ", "4 Of ", "5 Of ", "6 Of ", "7 Of ", "8 Of ", "9 Of ", "10 Of ", "Jack Of ", "Queen Of ", "King Of "};
+            // counter for the deck string array to add the card to an index
+        int counter = 0;
+            // for loop to create the deck
+        for(int i = 0; i < suites.length; i++){
+            for(int j = 0; j < rank.length; j++){
+                String card = rank[j] + suites[i];
+                deck[counter] = card;
+                counter++;
+            }
+        }
         return deck;
     }
 
